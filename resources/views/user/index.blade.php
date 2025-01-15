7ecb7f5e099828138d5ce70ab2cdde0ec608edfd
Ilya <sange0337@gmail.com>
2025-01-15 19:24:05 +0800

    make the home quality section

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -211,4 +211,88 @@
 
     </section>
 
+    <section class="section-primary">
+
+        <div class="md:flex md:items-start md:justify-between md:gap-10">
+            <div class="mb-6 md:flex-1">
+                <h2 class="text-xl font-bold tracking-wide md:text-2xl !leading-[120%] text-dark-black">Наши клиенты выбирают <br class="mb-1"> <span class="text-2xl golden-title md:text-4xl">качество</span></h2>
+            </div>
+
+            <div class="md:flex-1">
+                <p class="mb-6">мы понимаем, что строительство дома связано со стрессом: опасения по поводу процесса установки, стихийные бедстия, насекомые и другие факторы</p>
+                <p>наши дома решают эти роблемы, мы уважаем ваши потребности и гарантируем соблюдение высоких стандартов безопсности и функциональности.</p>
+            </div>
+        </div>
+
+        <div class="text-xl font-bold tracking-wide !leading-[120%] text-dark-black my-8 mb-4 md:mb-8 md:text-2xl md:font-normal">наше производство</div>
+
+        <div class="flex flex-wrap items-center gap-3 md:gap-x-10 md:gap-y-8">
+
+            <div class="p-4 rounded-xl border border-[#E5E3DB] flex flex-col justify-between h-60 md:h-80 sm:basis-[49%] md:basis-[48%] lg:basis-[28%]">
+
+                <div class="flex items-start justify-between gap-6">
+                    <p class="text-xl font-bold tracking-wide !leading-[120%] text-dark-black md:text-2xl">Древесина, полученная из экологически чистых источников</p>
+                    <div class="w-2 h-2 rounded-sm bg-green-primary shrink-0"></div>
+                </div>
+
+                <div class="text-sm md:text-base">
+                    премиум древесина, которую мы используем, поступает из лесов, управляемых в соответсвии с экологически устойчивыми практиками.
+                </div>
+            </div>
+
+            <div class="p-4 rounded-xl border border-[#E5E3DB] flex flex-col justify-between h-60 md:h-80 sm:basis-[49%] md:basis-[48%] lg:basis-[28%]">
+
+                <div class="flex items-start justify-between gap-6">
+                    <p class="text-xl font-bold tracking-wide !leading-[120%] text-dark-black md:text-2xl">Доступные цены</p>
+                    <div class="w-2 h-2 rounded-sm bg-green-primary shrink-0"></div>
+                </div>
+
+                <div class="text-sm md:text-base">
+                    мы ценим нашу репутацию и ожидания тех, кто нам доверяет. Нет скрытых платежей! Мы ценим ваше доверие и стремимся к полной прозрачности. Работаем по договору.
+                </div>
+            </div>
+
+            <div class="p-4 rounded-xl border border-[#E5E3DB] flex flex-col justify-between h-60 md:h-80 sm:h-80 sm:basis-[49%] md:order-1 md:basis-[38%] lg:basis-[28%]">
+
+                <div class="flex items-start justify-between gap-6">
+                    <p class="text-xl font-bold tracking-wide !leading-[120%] text-dark-black md:text-2xl">Удобная доставка до участка</p>
+                    <div class="w-2 h-2 rounded-sm bg-green-primary shrink-0"></div>
+                </div>
+
+                <div class="text-sm md:text-base">
+                    рубка и строительство на территории нашей площадки, что обеспечивает наблюдение за качеством работ и материала, четкое соблюдение технологии. Доставка к месту сборки автомобильным транспортом.
+                </div>
+            </div>
+
+            <div class="p-4 rounded-xl border border-[#E5E3DB] flex flex-col justify-between h-80 sm:basis-[49%] md:basis-[58%] lg:basis-[38%]">
+
+                <div class="flex items-start justify-between gap-6">
+                    <p class="text-xl font-bold tracking-wide !leading-[120%] text-dark-black md:text-2xl">Собственное производство и изготовление</p>
+                    <div class="w-2 h-2 rounded-sm bg-green-primary shrink-0"></div>
+                </div>
+
+                <div class="text-sm md:text-base">
+                    Мы берем на себя всю работу по проектированию и строительству – разрабатываем архитектурно-эскизный проект и рабочую документацию, реализуем полный цикл производства срубов, завозим материалы на стройплощадку, собираем конструкцию, проводим отделочные работы и сдаем готовый объект. Наша команда состоит из архитекторов, инженеров-конструкторов и инженеров по дереву.
+                </div>
+            </div>
+
+            <div class="p-4 rounded-xl border border-[#E5E3DB] flex flex-col justify-between h-80 sm:basis-[49%] md:order-3 md:basis-[38%] lg:basis-[28%] lg:order-2">
+
+                <div class="flex items-start justify-between gap-6">
+                    <p class="text-xl font-bold tracking-wide !leading-[120%] text-dark-black md:text-2xl">Подходящий вариант для каждого</p>
+                    <div class="w-2 h-2 rounded-sm bg-green-primary shrink-0"></div>
+                </div>
+
+                <div class="text-sm md:text-base">
+                    каждый проект уникален, поэтому мы предлагаем разнообразные строительные решения, специально разработанные с учетом ваших потребностей, оптимизируя время и бюджет.
+                </div>
+            </div>
+
+            <div class="h-80 sm:basis-[49%] md:order-2 md:basis-[58%] lg:basis-[38%]">
+                <img class="object-cover object-center w-full h-full" src="{{ asset('images/home/home-quality.webp') }}" alt="">
+            </div>
+        </div>
+
+    </section>
+
 </x-user-layout>
