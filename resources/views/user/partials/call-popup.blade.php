90775ee334271e9c33af96d38a078510bc30b027
Ilya <sange0337@gmail.com>
2025-01-30 17:59:42 +0800

    make a call popup

--- resources/views/user/partials/call-popup.blade.php
+++ resources/views/user/partials/call-popup.blade.php
@@ -1,10 +1,10 @@
-<div x-data="{ open: false }" x-cloak x-transition x-show="open" class="fixed z-40 flex items-center gap-4 p-4 bg-white right-1/4 bottom-10">
+<div x-data="{ open: true }" x-cloak x-transition x-show="open" class="fixed z-40 flex items-center gap-4 p-4 bg-white right-1/4 bottom-10">
     <a href="" class="block p-2 rounded-full bg-[#25D366]">
         <img src="{{ asset('images/svgs/wp-popup.svg') }}" alt="">
     </a>
-    <a href="" class="block p-2 rounded-full bg-[#375A48]">
+    <button @click="$dispatch('open-form')" type="button" class="block p-2 rounded-full bg-[#375A48]">
         <img src="{{ asset('images/svgs/tel-popup.svg') }}" alt="">
-    </a>
+    </button>
     <button @click="open = false">
         <img src="{{ asset('images/svgs/close-popup.svg') }}" alt="">
     </button>
